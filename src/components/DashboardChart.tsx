cd62d5ade7ca9f46a42867c66765ba0462de22ab
Clay Mangum <clay@trinitasaccounting.com>
2025-03-19 16:31:34 -0400

    functioning line chart

--- src/components/DashboardChart.tsx
+++ src/components/DashboardChart.tsx
@@ -8,25 +8,25 @@ type Props = {
     loans: any[]
 }
 
-const dataset = [
+// const dataset = [
 
 
-    { date: new Date(2004, 0, 1), fr: 35093.824, gb: 34865.78, dl: 35528.715 },
-    { date: new Date(2005, 0, 1), fr: 35495.465, gb: 35623.625, dl: 36205.574 },
-    { date: new Date(2006, 0, 1), fr: 36166.16, gb: 36214.07, dl: 38014.137 },
-    { date: new Date(2007, 0, 1), fr: 36845.684, gb: 36816.676, dl: 39752.207 },
-    { date: new Date(2008, 0, 1), fr: 36761.793, gb: 36264.79, dl: 40715.434 },
-    { date: new Date(2009, 0, 1), fr: 35534.926, gb: 34402.36, dl: 38962.938 },
-    { date: new Date(2010, 0, 1), fr: 36086.727, gb: 34754.473, dl: 41109.582 },
-    { date: new Date(2011, 0, 1), fr: 36691, gb: 34971, dl: 43189 },
-    { date: new Date(2012, 0, 1), fr: 36571, gb: 35185, dl: 43320 },
-    { date: new Date(2013, 0, 1), fr: 36632, gb: 35618, dl: 43413 },
-    { date: new Date(2014, 0, 1), fr: 36527, gb: 36436, dl: 43922 },
-    { date: new Date(2015, 0, 1), fr: 36827, gb: 36941, dl: 44293 },
-    { date: new Date(2016, 0, 1), fr: 37124, gb: 37334, dl: 44689 },
-    { date: new Date(2017, 0, 1), fr: 37895, gb: 37782.83, dl: 45619.785 },
-    { date: new Date(2018, 0, 1), fr: 38515.918, gb: 38058.086, dl: 46177.617 },
-];
+//     { date: new Date(2004, 0, 1), fr: 35093.824, gb: 34865.78, dl: 35528.715 },
+//     { date: new Date(2005, 0, 1), fr: 35495.465, gb: 35623.625, dl: 36205.574 },
+//     { date: new Date(2006, 0, 1), fr: 36166.16, gb: 36214.07, dl: 38014.137 },
+//     { date: new Date(2007, 0, 1), fr: 36845.684, gb: 36816.676, dl: 39752.207 },
+//     { date: new Date(2008, 0, 1), fr: 36761.793, gb: 36264.79, dl: 40715.434 },
+//     { date: new Date(2009, 0, 1), fr: 35534.926, gb: 34402.36, dl: 38962.938 },
+//     { date: new Date(2010, 0, 1), fr: 36086.727, gb: 34754.473, dl: 41109.582 },
+//     { date: new Date(2011, 0, 1), fr: 36691, gb: 34971, dl: 43189 },
+//     { date: new Date(2012, 0, 1), fr: 36571, gb: 35185, dl: 43320 },
+//     { date: new Date(2013, 0, 1), fr: 36632, gb: 35618, dl: 43413 },
+//     { date: new Date(2014, 0, 1), fr: 36527, gb: 36436, dl: 43922 },
+//     { date: new Date(2015, 0, 1), fr: 36827, gb: 36941, dl: 44293 },
+//     { date: new Date(2016, 0, 1), fr: 37124, gb: 37334, dl: 44689 },
+//     { date: new Date(2017, 0, 1), fr: 37895, gb: 37782.83, dl: 45619.785 },
+//     { date: new Date(2018, 0, 1), fr: 38515.918, gb: 38058.086, dl: 46177.617 },
+// ];
 
 
 
@@ -35,7 +35,7 @@ export default function DashboardChart({ loans }: Props) {
 
 
     //Formatting our chart data
-    let loanDataSet: any[] = []
+    // let loanDataSet: any[] = []
     let allYearsData: { year: string, amount: number }[] = []
     const sumAmountsByYear = (loans: any) => {
         //loop through and get the sum for each year 
@@ -134,8 +134,8 @@ export default function DashboardChart({ loans }: Props) {
 
     }
 
-    const result = sumAmountsByYear(loans)
-    console.log(result)
+    sumAmountsByYear(loans)
+    // console.log(result)
 
 
     return (
